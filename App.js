fb3e29f35505e497e76010eeddb83103e97769d1
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2023-02-16 15:13:25 -0300

    Created the expo react native repository

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color: "#FFF",
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "#fff"
+  }
 });
